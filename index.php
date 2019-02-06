6a856ff2c635d5635172b3c386ff5ca65ed7f50f
bart van der kieft <bartvanderkieft@outlook.com>
2019-02-06 09:04:49 +0100

    plaatjes zijn gelinked

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 </head>
 <body>
 	<div class="background">
-			<img class=grp4smurf src="img/4 smurf los.png">
-			<img class=grp5smurf src="img/5 smurf los.png">
-			<img class=grp6smurf src="img/6 smurf los.png">
+			<a href=groep4.php><img class=grp4smurf src="img/4 smurf los.png"></a>
+			<a href="groep5.php"><img class=grp5smurf src="img/5 smurf los.png"></a>
+			<a href="groep6.php"><img class=grp6smurf src="img/6 smurf los.png"></a>
 	</div>
 </body>
 </html>
